55c52c5d11773c44104dedbdd08539632679fb98
usernameisyuan <2208521620@qq.com>
2024-05-19 15:49:42 +0800

    revise

--- ptuning/web_demo.bat
+++
@@ -1,5 +0,0 @@
- python web_demo.py^
-    --model_name_or_path D:\work\ChatGLM2-6B\model ^
-    --ptuning_checkpoint D:\work\ChatGLM2-6B\ptuning\output\adgen-chatglm2-6b-pt-LR1\checkpoint-1000^
-    --pre_seq_len 128
-
